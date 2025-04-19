c3fe3470d2fa4d9a56f38da12b41eb5ab9cf8f5d
Eli P <ermparto@gmail.com>
2025-04-19 19:09:28 +0200

    Start on vectorized solutions and fitnesses

---
+++ Experiments/Inherited knowledge multiparam (vectorized solutions)/config.py
@@ -0,0 +1,19 @@
+"""
+Configuration parameters
+"""
+
+# Body
+DATABASE_FILE = "database.sqlite"
+NUM_REPETITIONS_BODY = 1 # No. of repetitions of the morphology optimization process
+POPULATION_SIZE_BODY = 8
+OFFSPRING_SIZE = 4
+NUM_GENERATIONS_BODY = 5 # No. of body optimization generations
+
+# Differential Evolution parameters
+NUM_SIMULATORS_BRAIN = 8
+NUM_GENERATIONS_BRAIN = 3
+NUM_POPULATION_BRAIN = 8
+PERTURB_SD_MOD = 2
+P_CR = 0.7 # [0,1]
+P_MU = 0.5 # [0,1]
+F = 1.25 # [0,2]
